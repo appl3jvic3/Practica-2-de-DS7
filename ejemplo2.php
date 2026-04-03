e561c7ea9c80d5c2b2b323fe02a2ea5a17f2414d
Daniel_ecc <dancord12072002@gmail.com>
2026-04-03 15:32:52 -0500

    Adicion de archivos css y modificaciones

--- ejemplo2.php
+++ ejemplo2.php
@@ -1,15 +1,28 @@
- <?php
+<!DOCTYPE html>
+<html lang="es">
 
-    $equipo = array(
-        'portero' => 'Cech',
-        'defensa' => 'Terry',
-        'medio' => 'Lampard',
-        'delantero' => 'Torres'
-    );
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo 2</title>
+    <link rel="stylesheet" href="ejemplo2.css">
+</head>
 
+<body>
+    <main class="wrap">
+        <?php
+        $equipo = array(
+            'portero' => 'Cech',
+            'defensa' => 'Terry',
+            'medio' => 'Lampard',
+            'delantero' => 'Torres'
+        );
 
-    foreach ($equipo as $posicion => $jugador) {
+        foreach ($equipo as $posicion => $jugador) {
+            echo "El <strong>" . $posicion . "</strong> es " . $jugador . "<br>";
+        }
+        ?>
+    </main>
+</body>
 
-        echo "El <strong>" . $posicion . "</strong> es " . $jugador . "<br>";
-    }
-    ?>
+</html>
