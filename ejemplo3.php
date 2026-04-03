4efe944dbb78e0764dbe77ebf90061b8b34adc34
Daniel_ecc <dancord12072002@gmail.com>
2026-04-03 15:49:42 -0500

    Adicion de html en el ejemplo 3.php

--- ejemplo3.php
+++ ejemplo3.php
@@ -1,12 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
 
-if (file_exists("colores.xml")) {
-    $xml = simplexml_load_file("colores.xml");
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo 3</title>
+    <link rel="stylesheet" href="./ejemplo3.css">
+</head>
 
+<body>
+    <main class="wrap">
+        <?php
+        if (file_exists("colores.xml")) {
+            $xml = simplexml_load_file("colores.xml");
 
-    foreach ($xml->color as $color) {
-        echo "El color <strong>" . $color->nombre . "</strong> tiene el código hexadecimal <strong>" . $color->codigo_hexadecimal . "</strong>.<br>";
-    }
-} else {
-    echo "Error: No se encontró el archivo colores.xml en esta carpeta.";
-}
+            foreach ($xml->color as $color) {
+                echo "El color <strong>" . $color->nombre . "</strong> tiene el código hexadecimal <strong>" . $color->codigo_hexadecimal . "</strong>.<br>";
+            }
+        }
+
+        ?>
+    </main>
+</body>
+
+</html>
